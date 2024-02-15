fd20fef12588c35f0fa41ec2ccebb23d384c6c03
König <harald.koenig@fhdw.de>
2024-02-15 14:23:03 +0100

    Initial Commit

--- src/facade/ApplicationFacade.java
+++ src/facade/ApplicationFacade.java
@@ -8,26 +8,14 @@ import lexer.main.LexerImpl;
 import parser.ExpressionParser;
 import parser.ExpressionParserProxy;
 /**
- * Fassaden-Klasse in die Applikation
- * Enthält nur die für Verwenderprogramme relevanten (veröffentlichten) Operationen
+ * Facade class only contains relevant services for clients
  */
 public class ApplicationFacade {
-/**	 
- * @param input: Der arithmetische Ausdruck als Zeichenkette, z.B. "6*(10+9)"
- * @return Der Syntaxbaum für den eingegebenen Ausdruck, wenn die Syntax korrekt war
- * @throws ParserException, wenn die Syntax nicht korrekt war, z.B. "6*(10+9"
- */
 	public Expression checkSyntax(String input) throws ParserException {
 		Lexer l = new LexerImpl();
 		ExpressionParserProxy ep = new ExpressionParserProxy();
 		return ep.toExpression(l.toTokenSequence(input));
 	}
-/**	
- * @param input: Der arithmetische Ausdruck als Zeichenkette, z.B. "6*(10+9)"
- * @return Den Wert der Eingabe, z.B. 114 (für obigen input) 
- * @throws CalculationException, falls durch Null dividiert wurde
- * @throws ParserException, wenn die Syntax nicht korrekt war, z.B. "6*(10+9"
- */
 	public Integer evaluate(String input) throws CalculationException, ParserException {
 		return this.checkSyntax(input).evaluate();
 	}
